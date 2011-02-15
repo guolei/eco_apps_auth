d7a6768edc53c7e16440a13e2128e1fa523aa867
Guo Lei <guolei@idapted.com>
2011-02-15 17:40:59 +0800

    bug fix of set before_filter

--- lib/eco_apps_auth.rb
+++ lib/eco_apps_auth.rb
@@ -11,19 +11,17 @@ module EcoAppsAuth
     initializer "include_hacks" do
       ActionController::Base.send(:include, EcoAppsAuth::Authenticate)
       ActionView::Base.send(:include, ::AuthViewsHelper)
-    end
-    
-    initializer "reset_rights", :after=> :disable_dependency_loading do
-      RightService.reset_rights if Rails.env.production?
-    end
-    
-    config.after_initialize do  
+      
       if Rails.env.production?
         ActionController::Base.before_filter :login_required
         ActionController::Base.before_filter :check_access_right
       elsif Rails.env.development?
         ActionController::Base.before_filter :login_required
       end
+    end
+    
+    initializer "reset_rights", :after=> :disable_dependency_loading do
+      RightService.reset_rights if Rails.env.production?
     end
   
   end
